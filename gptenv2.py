e71142698f88ef47b4be5cc0afefec05ca3c5f16
Prithvi Maram <156536904+prithvi-gitforcetalent@users.noreply.github.com>
2025-02-13 10:30:18 +0530

    Update gptenv2.py

--- gptenv2.py
+++ gptenv2.py
@@ -18,9 +18,11 @@ else:
     client = OpenAI(api_key=api_key)
 
     # Input: Dropdown with OpenAI GPT models
-    models = [
+       models = [
         "gpt-3.5-turbo",
-        # "gpt-4",
+        "gpt-4o-mini",
+        "gpt-4o",
+        # "gpt-4",st
         # "gpt-4-turbo",
         # "gpt-4-32k",
         # "gpt-4-1106-preview"
